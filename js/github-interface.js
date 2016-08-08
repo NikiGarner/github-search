b88a14a8a4bb7781612cf561a8b192ca660c9b8b
Niki Garner <nbarfield5@gmail.com>
2016-08-07 21:53:48 -0700

    display search results

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,53 +1,13 @@
-// 
-// var getRepos = require("./../js/github.js").getRepos;
-//
-// $(function(){
-//   $("#btn").submit(function(event){
-//     event.preventDefault();
-//
-//     var search = $("#name").val();
-//
-//     // $(".showRepo").empty();
-//     // $(".showName").empty();
-//     getRepos(search);
-//
-//     $("input#name").val("");
-//
-//   });
-//
-// });
-//
-// var Repo = require("./../js/github.js").Repo;
-//
-// $(document).ready(function() {
-//
-//   var currentRepoObject = new Repo();
-//
-//   $("form#input").submit(function(event) {
-//     event.preventDefault();
-//
-//     var username = $("#ghUsername").val();
-//     $("#ghUsername").val("");
-//
-//     currentRepoObject.getRepos(username);
-//
-//   });
-// });
-//
-//
-// var getRepos = require("./../js/github.js").getRepos;
-//
-// $(document).ready(function() {
-// $("form#search-form").click(function(event){
-//   event.preventDefault();
-//
-//   var search = $("input#human-search").val();
-//
-//   $("#repos").empty();
-//   $("#search-name").empty();
-//
-//   getRepos(search);
-//
-//   $("input#human-search").val("");
-// });
-// });
+var User = require('./../js/github.js').userModule;
+
+$(document).ready(function(){
+  alert("test");
+  $(".search").submit(function(event){
+    event.preventDefault();
+    alert('test2');
+    $(".results").empty();
+    var username = $("#username").val();
+    var newUser = new User(username);
+    newUser.getRepos(newUser.username);
+  });
+});
